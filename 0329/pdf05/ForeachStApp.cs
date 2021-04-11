21bb43bc3764236b077fc8bcc56ce8a809c84c6e
alsrb65321172 <80227793+alsrb65321172@users.noreply.github.com>
2021-04-12 03:06:23 +0900

    Add files via upload

---
+++ 0329/pdf05/ForeachStApp.cs
@@ -0,0 +1,13 @@
+namespace ForeachStApp
+{
+class Program
+{
+static void Main( string [] args)
+{
+string [] color = { " red", " green", "blue");
+foreach(string s in color);
+Console.WriteLine(s);
+(
+}
+}
+}
